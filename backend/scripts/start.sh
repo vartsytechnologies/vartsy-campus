ebc0f2be72cc8f36097bf730c2a05886f74f369f
luornor <tetteynathan89@gmail.com>
2025-11-20 12:52:34 +0000

    modified:   backend/compose.yaml 	modified:   backend/scripts/start.sh

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -3,4 +3,7 @@ set -e
 ./scripts/wait_for_db.sh
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn vcampus.wsgi:application --bind 0.0.0.0:8000 --workers 3
+
+# Use $PORT if defined, otherwise default to 8000
+gunicorn vcampus.wsgi:application --bind 0.0.0.0:${PORT:-8001} --workers 3
+    
