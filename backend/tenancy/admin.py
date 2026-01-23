9514b69107571543e753637a8d10fd2cc4bfcf96
luornor <tetteynathan89@gmail.com>
2026-01-23 12:29:15 +0000

    modified:   backend/accounts/admin.py 	modified:   backend/tenancy/admin.py

--- backend/tenancy/admin.py
+++ backend/tenancy/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import SchoolDomain, Membership, School
 # Register your models here.
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'schema_name', 'plan', 'paid_until', 'on_trial')
+    search_fields = ('name', 'schema_name', 'plan')
+    list_filter = ('plan', 'on_trial')
+    
+@admin.register(SchoolDomain)
+class SchoolDomainAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'tenant', 'is_primary')
+    search_fields = ('domain',)
+    list_filter = ('is_primary',)
+    
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'school', 'role', 'is_active')
+    search_fields = ('user__email', 'school__name', 'role')
+    list_filter = ('role', 'is_active')
