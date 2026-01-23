f52894ccb451dfba3d7d6154e8895b5ac5b19f17
luornor <tetteynathan89@gmail.com>
2026-01-23 12:08:31 +0000

    modified:   backend/accounts/admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -7,7 +7,7 @@ CustomUser = get_user_model()
 @admin.register(get_user_model())
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active', 'is_email_verified')
+    list_display = ('email','first_name', 'last_name','role','auth_provider' 'is_staff', 'is_active', 'is_email_verified')
     list_filter = ('is_staff', 'is_active', 'is_email_verified')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
