5c53a0980c4f448f3949e524254f93718519a075
luornor <tetteynathan89@gmail.com>
2026-01-26 22:15:55 +0000

    modified:   backend/accounts/urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from .views import(
     ChangePasswordView,CsrfBootstrapView,MeView,LoginView,
-    RegisterAPIView,CookieTokenRefreshView,
-    LogoutView,MeView,ForgotPasswordView,
-    GoogleOneTapView, EmailVerificationView
+    RegisterAPIView,CookieTokenRefreshView,ResendEmailVerificationView,
+    LogoutView,MeView,ForgotPasswordView,GoogleOneTapView, EmailVerificationView
 )
 from .views import CsrfBootstrapView
 
@@ -12,6 +11,7 @@ urlpatterns = [
     # Authentication
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('verify-email/',EmailVerificationView.as_view(), name='verify_email'),
+    path('resend-verification/',ResendEmailVerificationView.as_view(), name='resend_verification'),
     path('login/', LoginView.as_view(), name='login'),
     path('refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
